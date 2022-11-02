7889bc4e5687e6b9d2b91539b70e636099d9cbae
Philipp Dormann <philipp@philippdormann.de>
2022-11-02 22:32:18 +0100

    api: add redis cache

--- api.js
+++ api.js
@@ -5,6 +5,12 @@ const app = Fastify()
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 
+let redisclient = undefined
+if (process.env.CACHE_REDIS_URL) {
+  const Redis = require('ioredis');
+  redisclient = new Redis(process.env.CACHE_REDIS_URL);
+}
+
 const schema = `
   type Allergen {
     short: String
@@ -26,9 +32,26 @@ const schema = `
     sources: [String]
   }
 `;
+
+async function getFoodItemsWithCache({ kw, source }) {
+  if (process.env.CACHE_REDIS_URL) {
+    let result = await redisclient.get(`${kw}:${source}`)
+    if (result) {
+      result = JSON.parse(result);
+    } else {
+      result = await getFoodItems({ kw, source });
+      await redisclient.set(`${kw}:${source}`, JSON.stringify(result));
+      await redisclient.expire(`${kw}:${source}`, 300);
+    }
+    return result;
+  } else {
+    return await getFoodItems({ kw, source });
+  }
+}
+
 const resolvers = {
   Query: {
-    food: (_, { kw, source }) => getFoodItems({ kw, source }),
+    food: (_, { kw, source }) => getFoodItemsWithCache({ kw, source }),
     sources: () => getSources()
   }
 }
