4841fb0cc2c2dfb71b1890e0cabcc432f25fb75e
Philipp Dormann <philipp@philippdormann.de>
2022-11-07 11:05:19 +0100

    fix: inetmenue workspace: usage in api sample

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,6 +1,6 @@
 const Fastify = require('fastify')
 const mercurius = require('mercurius');
-const { getFoodItems, getSources } = require('../lib/index');
+const { getFoodItems, getSources } = require('inetmenue');
 const app = Fastify()
 require("dotenv").config();
 const port = process.env.PORT || 3000;
